a76a698e066a82e03511eb3e03994724ae54f1c7
OscarDelamao <g.omar.deen@gmail.com>
2026-07-22 16:27:10 +1000

    Update cache version from v10 to v11

--- forge-sw.js
+++ forge-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'the-forge-v10';
+const CACHE = 'the-forge-v11';
 const CORE = ['./index.html', './forge.webmanifest', './forge-icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
