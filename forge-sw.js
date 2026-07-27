108ef88584204eddf872ca928da9d1ce1c5df87a
OscarDelamao <g.omar.deen@gmail.com>
2026-07-27 10:17:24 +1000

    Update cache version from v14 to v15

--- forge-sw.js
+++ forge-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'the-forge-v14';
+const CACHE = 'the-forge-v15';
 const CORE = ['./index.html', './forge.webmanifest', './forge-icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
